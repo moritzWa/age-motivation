7e255809344de0c40de545b85b77ba49075f1ca0
moritzWa <wallawitsch@gmail.com>
2024-02-05 13:28:45 -0800

    add basic counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const birthdate = new Date("1998-12-13");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAge((Date.now() - birthdate.getTime()) / 1000 / 60 / 60 / 24 / 365);
+    }, 10);
+    return () => clearInterval(interval);
+  }, [birthdate]);
+
   return (
-    <div className="App">
-      <header className="App-header">app here</header>
+    <div className="flex h-screen m-auto mt-32 font-medium max-w-7xl">
+      <div className="flex flex-col w-1/2">
+        <div className="text-6xl">{age.toFixed(9)}</div>
+        <div className="flex-1 h-full">calendar</div>
+      </div>
+      <div className="w-1/2">quotes</div>
     </div>
   );
 }
