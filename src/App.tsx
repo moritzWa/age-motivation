688365400f309400a4a21635adb52f117e24cd15
moritzWa <wallawitsch@gmail.com>
2024-02-05 17:09:30 -0800

    show life weeks calendar

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,57 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setAge((Date.now() - birthdate.getTime()) / 1000 / 60 / 60 / 24 / 365);
-    }, 10);
+    }, 1000);
     return () => clearInterval(interval);
   }, [birthdate]);
 
+  const lifeExpectancy = 85;
+  const totalWeeksInGrid = 98 * 52;
+  const weeksUntilBirthdayInFirstYearAlive = Math.floor(
+    (new Date().getTime() -
+      new Date(new Date().getFullYear(), 0, 1).getTime()) /
+      1000 /
+      60 /
+      60 /
+      24 /
+      7
+  );
+  const weeksUsed = Math.floor(age * 52);
+  const weeksLeft = lifeExpectancy * 52 - weeksUsed;
+
   return (
-    <div className="flex h-screen m-auto mt-32 font-medium max-w-7xl">
-      <div className="flex flex-col w-1/2">
-        <div className="text-6xl">{age.toFixed(9)}</div>
-        <div className="flex-1 h-full">calendar</div>
+    <div className="grid grid-cols-2 gap-4 px-5 m-auto mt-32 mb-12 max-w-7xl">
+      <div className="flex flex-col">
+        <div className="mb-5 text-6xl font-medium">{age.toFixed(9)}</div>
+        <div
+          className="grid"
+          style={{
+            gridTemplateColumns: "repeat(52, 1fr)",
+            // gridTemplateRows: "repeat(98, 1fr)",
+          }}
+        >
+          {/* {map small div for every week that is left */}
+          {/* {Array.from({ length: totalWeeksInGrid }, (_, i) => (
+            <div key={i} className="border border-white bg-slate-300 h-[7px]" />
+          ))} */}
+          {Array.from(
+            { length: weeksUntilBirthdayInFirstYearAlive },
+            (_, i) => (
+              <div
+                key={i}
+                className="border border-white bg-slate-300 h-[8px]"
+              />
+            )
+          )}
+          {Array.from({ length: weeksUsed }, (_, i) => (
+            <div key={i} className="border border-white bg-slate-500 h-[8px]" />
+          ))}
+          {Array.from({ length: weeksLeft }, (_, i) => (
+            <div key={i} className="border border-white bg-slate-300 h-[8px]" />
+          ))}
+        </div>
       </div>
-      <div className="w-1/2">quotes</div>
+      <div className="w-full">bla</div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  extend: {
+    gridTemplateColumns: {
+      // Not necessary if you're not using gaps
+      "no-gap": "repeat(12, minmax(0, 1fr))",
+    },
+    gridTemplateRows: {
+      // Not necessary if you're not using gaps
+      "no-gap": "repeat(12, minmax(0, 1fr))",
+    },
   },
   plugins: [],
 };
