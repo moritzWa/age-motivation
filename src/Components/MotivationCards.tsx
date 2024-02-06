4ddf6a7d7585175101e37ce5ec904378ee866d6b
moritzWa <wallawitsch@gmail.com>
2024-02-05 21:18:11 -0800

    add some cards

--- src/Components/MotivationCards.tsx
+++ src/Components/MotivationCards.tsx
@@ -21,10 +21,52 @@ const cards: MotivationCardsType[] = [
     verb: "releases",
     what: "College Dropout",
   },
+  {
+    age: 19,
+    name: "Mark Zuckerberg",
+    verb: "launches",
+    what: "Facebook",
+  },
+  {
+    age: 20,
+    name: "Bill Gates",
+    verb: "founds",
+    what: "Microsoft",
+  },
+  {
+    age: 27,
+    name: "Elon Musk",
+    verb: "sold",
+    what: "Zip2",
+  },
+  {
+    age: 28,
+    name: "Elon Musk",
+    verb: "co-founds",
+    what: "PayPal",
+  },
+  {
+    age: 25,
+    name: "Larry Page and Sergey Brin",
+    verb: "start",
+    what: "Google",
+  },
+  {
+    age: 21,
+    name: "Evan Spiegel",
+    verb: "co-founded",
+    what: "Snapchat",
+  },
+  {
+    age: 37,
+    name: "Travis Kalanick",
+    verb: "co-founds",
+    what: "Uber",
+  },
 ];
 
 const Card = ({ age, name, verb, what }: MotivationCardsType) => (
-  <div className="pb-4 text-gray-600 dark:text-gray-400 max-h-80">
+  <div className="pb-4 text-gray-600 dark:text-gray-300 max-h-80">
     <div>
       <span className="font-medium text-gray-900 dark:text-gray-100">
         {name}
@@ -41,7 +83,7 @@ const Card = ({ age, name, verb, what }: MotivationCardsType) => (
 
 function MotivationCards() {
   return (
-    <div className="">
+    <div className="h-full">
       {cards.map((c) => (
         <Card {...c} />
       ))}
