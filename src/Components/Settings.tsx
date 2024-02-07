740d3d6c63c6d14510153e5337658e2439bd0784
moritzWa <wallawitsch@gmail.com>
2024-02-07 11:19:36 -0800

    improve settings ui

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -16,36 +16,43 @@ const Settings: FC<SettingsProps> = ({ onSave, currentSettings }) => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-col items-center justify-center h-full text-gray-900 dark:text-gray-200"
-    >
-      <label className="mb-4">
-        Date of Birth:
-        <input
-          type="date"
-          value={birthdate}
-          onChange={(e) => setBirthdate(e.target.value)}
-          className="p-2 ml-2 text-gray-900 bg-white dark:bg-gray-800 dark:text-gray-200"
-        />
-      </label>
-      <label className="mb-4">
-        Life Expectancy:
-        <input
-          type="number"
-          value={lifeExpectancy}
-          onChange={(e) => setLifeExpectancy(parseInt(e.target.value))}
-          className="p-2 ml-2 text-gray-900 bg-white dark:bg-gray-800 dark:text-gray-200"
-        />
-      </label>
-      <div className="pb-5">Life expectancy canculator</div>
-      <button
-        type="submit"
-        className="px-4 py-2 text-white bg-blue-500 rounded"
+    <div className="flex items-start justify-center w-full h-full">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center h-full text-gray-900 dark:text-gray-200 max-w-80"
       >
-        Save
-      </button>
-    </form>
+        <label className="mb-4">
+          Date of Birth:
+          <input
+            type="date"
+            value={birthdate}
+            onChange={(e) => setBirthdate(e.target.value)}
+            className="p-2 ml-2 text-gray-900 bg-white border border-gray-300 dark:bg-gray-800 dark:text-gray-200"
+          />
+        </label>
+        <label className="mb-4">
+          Life Expectancy:
+          <input
+            type="number"
+            value={lifeExpectancy}
+            onChange={(e) => setLifeExpectancy(parseInt(e.target.value))}
+            className="p-2 ml-2 text-gray-900 bg-white border border-gray-300 dark:bg-gray-800 dark:text-gray-200 w-[123px]"
+          />
+        </label>
+        <a
+          className="pb-5"
+          href="https://www.blueprintincome.com/tools/life-expectancy-calculator-how-long-will-i-live/"
+        >
+          Life expectancy calculator
+        </a>
+        <button
+          type="submit"
+          className="px-4 py-2 text-white bg-blue-500 rounded"
+        >
+          Save
+        </button>
+      </form>
+    </div>
   );
 };
 
